b8f712f7ccbfad0f70d2e9c92a2b345932477bda
ruchi <ruchi.chauhan@bel-technology.com>
2016-07-05 16:05:24 +0530

    create add class and when we click delete button then put 1 value in database

---
+++ resources/views/class-name/index.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.header')
+
+@section('content')
+<div class=" col-md-9 category">
+
+    <h1>Classname <a href="{{ url('/class-name/create') }}" class="btn btn-primary btn-xs" title="Add New ClassName"><span class="glyphicon glyphicon-plus" aria-hidden="true"/></a></h1>
+    <div class="table">
+        <table class="table table-bordered table-striped table-hover">
+            <thead>
+                <tr>
+                    <th>S.No</th><th> Name </th><th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+            {{-- */$x=0;/* --}}
+            @foreach($classname as $item)
+                {{-- */$x++;/* --}}
+                <tr>
+                    <td>{{ $x }}</td>
+                    <td>{{ $item->name }}</td>
+                    <td>
+                        <a href="{{ url('/class-name/' . $item->id) }}" class="btn btn-success btn-xs" title="View ClassName"><span class="glyphicon glyphicon-eye-open" aria-hidden="true"/></a>
+                        <a href="{{ url('/class-name/' . $item->id . '/edit') }}" class="btn btn-primary btn-xs" title="Edit ClassName"><span class="glyphicon glyphicon-pencil" aria-hidden="true"/></a>
+                        {!! Form::open([
+                            'method'=>'DELETE',
+                            'url' => ['/class-name', $item->id],
+                            'style' => 'display:inline'
+                        ]) !!}
+                            {!! Form::button('<span class="glyphicon glyphicon-trash" aria-hidden="true" title="Delete ClassName" />', array(
+                                    'type' => 'submit',
+                                    'class' => 'btn btn-danger btn-xs',
+                                    'title' => 'Delete ClassName',
+                                    'onclick'=>'return confirm("Confirm delete?")'
+                            ));!!}
+                        {!! Form::close() !!}
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+        <div class="pagination-wrapper"> {!! $classname->render() !!} </div>
+    </div>
+
+</div>
+@endsection
---
+++ resources/views/class-name/show.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+
+    <h1>ClassName {{ $classname->id }}
+        <a href="{{ url('class-name/' . $classname->id . '/edit') }}" class="btn btn-primary btn-xs" title="Edit ClassName"><span class="glyphicon glyphicon-pencil" aria-hidden="true"/></a>
+        {!! Form::open([
+            'method'=>'DELETE',
+            'url' => ['classname', $classname->id],
+            'style' => 'display:inline'
+        ]) !!}
+            {!! Form::button('<span class="glyphicon glyphicon-trash" aria-hidden="true"/>', array(
+                    'type' => 'submit',
+                    'class' => 'btn btn-danger btn-xs',
+                    'title' => 'Delete ClassName',
+                    'onclick'=>'return confirm("Confirm delete?")'
+            ));!!}
+        {!! Form::close() !!}
+    </h1>
+    <div class="table-responsive">
+        <table class="table table-bordered table-striped table-hover">
+            <tbody>
+                <tr>
+                    <th>ID</th><td>{{ $classname->id }}</td>
+                </tr>
+                <tr><th> Name </th><td> {{ $classname->name }} </td></tr>
+            </tbody>
+        </table>
+    </div>
+
+</div>
+@endsection
